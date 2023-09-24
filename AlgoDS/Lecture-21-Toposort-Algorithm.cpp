eeb80b522f1ed2f7ce7c228542cd4ad6f0724c6a
Sane Aalam <saneraza78692@gmail.com>
2023-09-24 19:24:14 +0530

    Update the toposort code.CPP

--- AlgoDS/Lecture-21-Toposort-Algorithm.cpp
+++ AlgoDS/Lecture-21-Toposort-Algorithm.cpp
@@ -8,53 +8,62 @@ using namespace std;
 // student1 inter the class before student 2
 // task1 done before task 2 
 
+//* Those who can complete their DFS call put into the stack
+// remember one things when use this algorithm - 
+// whenever something apear before something,think about this particular algorithm
+
 
 class Solution
 {
     private:
-    void CallDFS(int node, vector<int>& vis, stack<int> & st, vector<int> adj[]){
+    // you have to understand this concept 
+    // call by reference 
+    // call by value 
+    void DFS(int node, vector<int> & vis,stack<int>&Stack,vector<int> adj[]){
         
-        // those who complete thier DFS call push into the stack
+        // you have to mark visted the node
         vis[node] = 1;
         
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                CallDFS(it,vis,st,adj);
+        for(auto connectedNode : adj[node]){
+            if(vis[connectedNode]  == 0){
+                DFS(connectedNode,vis,Stack,adj);
             }
         }
         
-        st.push(node);
+        // when DFS call complete push into the stack
+        // backtracking concept used here
+        Stack.push(node);
     }
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+	    vector<int> result;
+	    stack<int> Stack;
 	    vector<int> vis(V,0);
-	    stack<int> st;
 	    
-	    for(int node = 0; node < V; node++){
-	        if(!vis[node]){
-	            CallDFS(node,vis,st,adj);
+	    for(int node = 0; node<V; node++){
+	        if(vis[node] == 0){
+	            // call the dfs with stack
+	            DFS(node,vis,Stack,adj);
 	        }
 	    }
 	    
+	    // now you can push stack element into Result vector
 	    
-	    // copy the all the nodes into the vector with help of the stack 
-	    vector<int> result;
-	    while(!st.empty()){
-	        int node = st.top();
+	    while(!Stack.empty()){
+	        int node = Stack.top();
 	        result.push_back(node);
-	        st.pop();
+	        Stack.pop();
 	    }
 	    
 	    return result;
 	}
 };
 
-//{ Driver Code Starts.
 
-/*  Function to check if elements returned by user
+/*---------------------  Function to check if elements returned by user
 *   contains the elements in topological sorted form
 *   V: number of vertices
 *   *res: array containing elements in topological sorted form
@@ -100,3 +109,4 @@ int main() {
     
     return 0;
 }
+// } Driver Code Ends
